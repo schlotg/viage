65208bcb05f12e1ad84119491ba028e5e98b46ef
Greg Schlottmann <schlotg123@gmail.com>
2018-06-22 23:35:31 -0700

    added a detect function

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Component } from './core/component';
 export { Listener } from './core/listener';
 export { getRouter, createRouter } from './core/router';
 export { Service } from './core/service';
+export { isCompatible } from './core/detect-ie';
